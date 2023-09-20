66304d1c6a2fede1a1ff85cc080312d25ae725c2
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-09-20 09:17:37 +0300

    Show greeting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-
+import Greeting from './greeting';
 function App() {
   return (
     <div className="App">
-     <p>Random Greeting</p>
+      <Routes>
+        <Route exact path='/' element={<Greeting />} />
+      </Routes>
     </div>
   );
 }
